8f42bd54a174ba6b103483d96f2d8cf02bda2d34
as_abdik <asliddinabdiqodirov15@gmail.com>
2025-03-26 17:38:17 +0500

    log in

--- category/views.py
+++ category/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+def region(request):
+    if request.method == "POST":
+        form = RegionForm(request.POST)
+        print(request.POST)
+        print(form)
+        print(form.cleaned_date)
+        if form.is_valid():
+            form.save()
+            return redirect('region')
+    else:
+        form = RegionForm()
+    crx = {
+        'form': form
+    }
 
-# Create your views here.
+    return render(request, 'region.html')
+        # Create your views here.
